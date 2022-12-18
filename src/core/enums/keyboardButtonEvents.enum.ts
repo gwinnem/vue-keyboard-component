0b2659289072889c90213d67ef3299216691abd7
Geirr Winnem <g.winnem@ringana.com>
2022-12-18 10:28:52 +0100

    Added enums to replace magic strings

--- src/core/enums/keyboardButtonEvents.enum.ts
+++ src/core/enums/keyboardButtonEvents.enum.ts
@@ -1,5 +1,10 @@
 export enum EKeyboardButtonEvent {
+  ALT_CLICKED = `onAltClicked`,
+  BACKSPACE_CLICKED = `onBackspaceClicked`,
+  CAPS_CLICKED = `onCapsClicked`,
+  CTRL_CLICKED = `onCtrlClicked`,
   CLICK = `onButtonClick`,
   KEY_DOWN = `onButtonDown`,
   KEY_UP = `onButtonUp`,
+  SHIFT_CLICKED = `onShiftClicked`,
 }

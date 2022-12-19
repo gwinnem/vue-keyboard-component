ba6d5719d1b040d455d37ca9e0d93b42e6fed764
Geirr Winnem <g.winnem@ringana.com>
2022-12-19 11:30:56 +0100

    Added more buttons and linting package

--- src/core/ms-keyboards/ms-Danish.ts
+++ src/core/ms-keyboards/ms-Danish.ts
@@ -11,23 +11,23 @@ export default <ILayoutItem>{
     default: [
       `\u00BD 1 2 3 4 5 6 7 8 9 0 + \u00B4 {bksp}`,
       `{tab} q w e r t y u i o p \u00E5 \u00A8`,
-      `a s d f g h j k l \u00E6 \u00F8 '  {enter}`,
+      `a s d f g h j k l \u00E6 \u00F8 ' {enter}`,
       `{shift} < z x c v b n m , . - {shift}`,
-      `{alt} {space} {alt}`,
+      `{ctrlleft} {altleft} {space} {altright} {ctrlright}`,
     ],
     shift: [
       `\u00A7 ! \\" # \u00A4 % & / ( ) = ? \` {bksp}`,
       `{tab} Q W E R T Y U I O P \u00C5 ^`,
-      `A S D F G H J K L \u00C6 \u00D8 *  {enter}`,
+      `A S D F G H J K L \u00C6 \u00D8 * {enter}`,
       `{shift} > Z X C V B N M ; : _ {shift}`,
-      `{alt} {space} {alt}`,
+      `{ctrlleft} {altleft} {space} {altright} {ctrlright}`,
     ],
     alt: [
       `{empty} {empty} @ \u00A3 $ \u20AC {empty} { [ ] } {empty} | {bksp}`,
       `{tab} {empty} {empty} \u20AC {empty} {empty} {empty} {empty} {empty} {empty} {empty} {empty} ~`,
       `{empty} {empty} {empty} {empty} {empty} {empty} {empty} {empty} {empty} {empty} {empty} {empty} {enter}`,
       `{shift} \\ {empty} {empty} {empty} {empty} {empty} {empty} \u00B5 {empty} {empty} {empty} {shift}`,
-      `{alt} {space} {alt}`,
+      `{ctrlleft} {altleft} {space} {altright} {ctrlright}`,
     ],
   },
 };
